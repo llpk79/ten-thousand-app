07927e927c3dcc50a4146f33ad5b401331bb3e56
Paul <pkutrich@gmail.com>
2018-12-13 13:04:18 -0700

    Issue fixes and improvements.
    Fixes #1, Fixes #3, Fixes #5, Fixes #34, Fixes #35, Fixes #36.
    
    Changes background images because old pattern was hard to look at.
    Refactor Die class to DieScatter class.
    	Overwrites on_touch_down, on_touch_move, on_touch_up
    	Attempt at adding some interdice physics.
    Adds basic animation.
    Adds white space for improved readability.
    Removes unused IntroScreen
    Improves PlayerNameScreen logic adding num_players as a kivy property.
    
     On branch logical
     Changes to be committed:
    	modified:   GameApp.py
    	modified:   logic.py

--- GameApp.py
+++ GameApp.py
@@ -1,13 +1,15 @@
+# Copyright 2018 Paul Kutrich. All rights reserved.
 import kivy
 import random
 import re
 
 kivy.require('1.10.1')
 
-from kivy.app import App
-import kivy.clock
-from kivy.lang import Builder
 import kivy.graphics
+from kivy.animation import Animation, AnimationTransition
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty, ListProperty, NumericProperty, BooleanProperty
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
@@ -21,7 +23,7 @@ from kivy.uix.image import Image
 
 from collections import deque
 from media import sounds, die_images
-from logic import Game, HumanPlayer, ComPlayer
+from logic import Game
 
 
 Builder.load_string("""
@@ -29,7 +31,7 @@ Builder.load_string("""
     name: 'menu'
     FloatLayout:
         Image:
-            source: 'images/pattern.png'
+            source: 'images/tabletop5.jpg'
             allow_stretch: True
             keep_ratio: False
         Button:
@@ -42,7 +44,7 @@ Builder.load_string("""
     name: 'result'
     FloatLayout:
         Image:
-            source: 'images/pattern.png'
+            source: 'images/tabletop1.jpg'
             allow_stretch: True
             keep_ratio: False
         Button:
@@ -56,13 +58,11 @@ Builder.load_string("""
             pos_hint: {'x': .45, 'y': .09}
             on_press: root.exit()
 
-
 <PlayerNameScreen>
     name: 'name'
-    on_enter:  self.on_enter_call(root.manager.screens)
     FloatLayout:
         Image:
-            source: 'images/pattern.png'
+            source: 'images/tabletop4.jpg'
             allow_stretch: True
             keep_ratio: False
 
@@ -70,7 +70,7 @@ Builder.load_string("""
     name: 'number'
     FloatLayout:
         Image:
-            source: 'images/pattern.png'
+            source: 'images/tabletop3.jpg'
             allow_stretch: True
             keep_ratio: False
         Label:
@@ -99,7 +99,6 @@ Builder.load_string("""
             Line:
                 rectangle: self.x - 1, self.y - 1, self.width + 2, self.height + 2
 
-
 <Roll>:
     text: 'Roll'
     size_hint: (.15, .15)
@@ -113,7 +112,6 @@ Builder.load_string("""
     pos_hint: {'x': .75, 'y': .15}
     font_size: 50
     background_color: .75, .41, .03, 1
-
 
 <Base>:
     Image:
@@ -150,17 +148,12 @@ Builder.load_string("""
 """)
 
 
-class IntroScreen(Screen):
-    pass
-
-
 class IntInput(TextInput):
-
     def __init__(self, **kwargs):
         super(IntInput, self).__init__(**kwargs)
 
     def insert_text(self, substring, from_undo=False):
-        if re.match(r'[1-3]', substring):
+        if re.match(r'^[1-3]$', substring):
             return super(IntInput, self).insert_text(substring, from_undo=False)
 
 
@@ -171,6 +164,7 @@ class PlayerNumberScreen(Screen):
         try:
             self.num_players = int(text)
             self.parent.current = 'name'
+
         except ValueError as e:
             print(e)
 
@@ -178,58 +172,58 @@ class PlayerNumberScreen(Screen):
 class FocusInput(TextInput):
     def __init__(self, **kwargs):
         super(FocusInput, self).__init__(**kwargs)
+
         self.focus = True
         self.write_tab = False
 
     def on_focus(self, widget, value):
-        if self.focus:
-            pass
-        elif self.parent and not self.focus:
+        if self.parent and not self.focus:
             self.parent.add_player(self, value)
 
 
 class PlayerNameScreen(Screen):
     player_names = ListProperty()
     active_game = ObjectProperty()
+    num_players = NumericProperty()
 
-    def get_num_players(self, screen_list):
-        for screen in screen_list:
+    def get_num_players(self):
+        for screen in self.parent.screens:
             if screen.name is 'number':
-                return screen.num_players
-
-    def on_enter_call(self, screen_list):
-        num_players = self.get_num_players(screen_list)
-        player_num = num_players
-        for i in range(1, num_players + 1):
-            self.add_widget(Label(text=f'Enter Player {player_num}\'s name:',
-                                  pos_hint={'x': .2, 'y': (i / 10) + .35},
-                                  size_hint=(.25, .05),
-                                  id=str(player_num)))
-            self.add_widget(FocusInput(multiline=False,
-                                       pos_hint={'x': .25, 'y': (i / 10) + .3},
-                                       size_hint=(.25, .05)))
-            player_num -= 1
+                self.num_players = screen.num_players
 
     def add_player(self, name, valid):
-        print('beep', name.text, valid)
         if not valid and name.text != '':
-            num_players = self.get_num_players(self.parent.screens)
             self.player_names.append(name.text)
-            if len(self.player_names) == num_players:
+
+            if len(self.player_names) == self.num_players:
                 self.active_game = Game(self.player_names)
                 self.parent.current = 'game'
 
+    def on_enter(self):
+        self.get_num_players()
+        player_num = self.num_players
+
+        for i in range(1, self.num_players + 1):
+            self.add_widget(Label(text=f'Enter Player {player_num}\'s name:',
+                                  pos_hint={'x': .2, 'y': (i / 10) + .45},
+                                  size_hint=(.25, .05),
+                                  id=str(player_num)))
+
+            self.add_widget(FocusInput(multiline=False,
+                                       pos_hint={'x': .25, 'y': (i / 10) + .4},
+                                       size_hint=(.25, .05)))
+            player_num -= 1
+
 
 class MenuScreen(Screen):
-
     def __init__(self, **kwargs):
         super(MenuScreen, self).__init__(**kwargs)
 
 
 class PlayerScore(BoxLayout):
-
     def __init__(self, **kwargs):
         super(PlayerScore, self).__init__(**kwargs)
+
         self.add_widget(Label(id='name'))
         self.add_widget(Label(id='round'))
         self.add_widget((Label(id='total')))
@@ -255,32 +249,31 @@ class GameScreen(Screen):
             self.list_o_players.append(player)
         return self.list_o_players
 
-    def on_enter(self, *args):
-        self.get_active_game()
-        self.get_active_game_players()
-        self.set_current_player()
-        score_area = BoxLayout(size_hint=(1, .05),
-                               pos_hint={'x': 0, 'y': .9},
-                               orientation='horizontal',
-                               id='score_area')
-        for i, player in enumerate(reversed(self.list_o_players)):
-            new = PlayerScore(id=player.name)
+    def set_current_player(self):
+        if self.current_player is not None:
+            self.update_round_score(red=True)
+            self.update_total_score()
+            self.current_player.round_score = 0
+            self.update_display('round')
+            self.ids['die_basket'].keepers.clear()
+            self.ids['die_basket'].valid_basket = [1, 0, 0, 1]
+            self.ids['roll'].keeper_count = 0
+            self.ids['roll'].background_color = [.21, .45, .3, .5]
+            self.update_display('name', 'small')
 
-            for child in new.children:
-                if child.id == 'name':
-                    child.text = player.name
-                    if i == 0:
-                        child.bold = True
-                        child.font_size = 25
-                    child.texture_update()
-                    child.size_hint = (child.texture_size[0] / 30, 1)
-                if child.id == 'round':
-                    child.text = f'Round: {str(player.round_score)}'
-                if child.id == 'total':
-                    child.text = f'Total: {str(player.total_score)}'
+        if not any([player.total_score > 2000 for player in self.list_o_players]):
+            temp = self.list_o_players.popleft()
+            self.current_player = temp
+            self.update_display('name', 'big')
+            self.list_o_players.append(temp)
 
-            score_area.add_widget(new)
-        self.add_widget(score_area)
+        else:
+            self.list_o_winners.append(self.current_player)
+            self.current_player = self.list_o_players.popleft()
+            self.update_display('name', 'big')
+
+            if not self.list_o_players:
+                self.parent.current = 'result'
 
     def update_round_score(self, red=None):
         if self.ids['die_basket'].valid_basket == [.4, .69, .3, 1]:
@@ -288,6 +281,7 @@ class GameScreen(Screen):
             self.current_player.round_score += curr_basket_score
             self.update_display('round')
             self.ids['die_basket'].basket_score = 0
+
             if not red:
                 self.ids['die_basket'].valid_basket = [1, 0, 0, 1]
                 self.ids['roll'].background_color = [.21, .45, .3, .5]
@@ -298,19 +292,23 @@ class GameScreen(Screen):
                     for kid in child.children:
                         if kid.id == self.current_player.name:
                             for imp in kid.children:
+
                                 if imp.id == 'round':
                                     if score_type == 'basket':
                                         imp.text = 'Round: {}'.format(str(self.current_player.round_score +
                                                                           self.ids['die_basket'].basket_score))
                                     if score_type == 'round':
                                         imp.text = f'Round: {str(self.current_player.round_score)}'
+
                                 elif imp.id == 'total':
                                     if score_type == 'total':
                                         imp.text = f'Total: {str(self.current_player.total_score)}'
+
                                 elif imp.id == 'name':
                                     if font == 'big':
                                         imp.font_size = 25
                                         imp.bold = True
+
                                     elif font == 'small':
                                         imp.font_size = 15
                                         imp.bold = False
@@ -330,28 +328,37 @@ class GameScreen(Screen):
         self.update_display('total')
         self.ids['dice'].update_dice(0)
 
-    def set_current_player(self):
-        if self.current_player is not None:
-            self.update_round_score(red=True)
-            self.update_total_score()
-            self.current_player.round_score = 0
-            self.update_display('round')
-            self.ids['die_basket'].keepers.clear()
-            self.ids['die_basket'].valid_basket = [1, 0, 0, 1]
-            self.ids['roll'].keeper_count = 0
-            self.ids['roll'].background_color = [.21, .45, .3, .5]
-            self.update_display('name', 'small')
-        if not any([player.total_score > 2000 for player in self.list_o_players]):
-            temp = self.list_o_players.popleft()
-            self.current_player = temp
-            self.update_display('name', 'big')
-            self.list_o_players.append(temp)
-        else:
-            self.list_o_winners.append(self.current_player)
-            self.current_player = self.list_o_players.popleft()
-            self.update_display('name', 'big')
-            if not self.list_o_players:
-                self.parent.current = 'result'
+    def on_enter(self, *args):
+        self.get_active_game()
+        self.get_active_game_players()
+        self.set_current_player()
+
+        score_area = BoxLayout(size_hint=(1, .05),
+                               pos_hint={'x': 0, 'y': .9},
+                               orientation='horizontal',
+                               id='score_area')
+
+        for i, player in enumerate(reversed(self.list_o_players)):
+            new = PlayerScore(id=player.name)
+
+            for child in new.children:
+                if child.id == 'name':
+                    child.text = player.name
+
+                    if i == 0:
+                        child.bold = True
+                        child.font_size = 25
+                    child.texture_update()
+                    child.size_hint = (child.texture_size[0] / 30, 1)
+
+                if child.id == 'round':
+                    child.text = f'Round: {str(player.round_score)}'
+
+                if child.id == 'total':
+                    child.text = f'Total: {str(player.total_score)}'
+
+            score_area.add_widget(new)
+        self.add_widget(score_area)
 
 
 class ResultsScreen(Screen):
@@ -368,6 +375,7 @@ class ResultsScreen(Screen):
     def on_enter(self, *args):
         self.get_winners()
         winners = sorted(self.winners, key=lambda x: x.total_score, reverse=True)
+
         self.add_widget(Label(text=f'{winners[0].name} wins !!\n\nWith {winners[0].total_score} points!',
                               font_size=40,
                               bold=True,
@@ -380,40 +388,74 @@ class ResultsScreen(Screen):
     def exit(self):
         pass
 
-class Die(Widget):
+
+class DieScatter(Scatter):
+    touch = ObjectProperty()
+
     def __init__(self, **kwargs):
-        super(Die, self).__init__(**kwargs)
+        super(DieScatter, self).__init__(**kwargs)
+        self.trigger = Clock.create_trigger(self.update_pos)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.scale += .1
+            touch.grab(self)
+            self.touch = touch
+            return True
+
+    def on_touch_move(self, touch):
+        if touch.grab_current is self:
+            self.to_widget(*touch.pos)
+            self.trigger()
+            # Clock.schedule_once(self.update_other, 0)
+
+    def update_pos(self, thing):
+        self.pos = (self.pos[0] + self.touch.dpos[0], self.pos[1] + self.touch.dpos[1])
+
+    def update_other(self, thing):
+        for child in self.parent.children:
+            if child.collide_widget(self):
+                child.pos = (child.pos[0] + self.touch.dpos[0], child.pos[1] + self.touch.dpos[1])
 
     def on_touch_up(self, touch):
-        if self.collide_point(*touch.pos):
-            for widget in self.parent.walk():
-                if isinstance(widget, Scatter):
-                    die_basket = self.parent.parent.parent.die_basket
-                    if widget.collide_widget(die_basket) and widget not in die_basket.keepers:
-                        die_basket.keepers.append(widget)
-                    if not widget.collide_widget(die_basket) and widget in die_basket.keepers:
-                        die_basket.keepers.remove(widget)
+        if touch.grab_current is self:
+            self.scale -= .1
+            die_basket = self.parent.parent.die_basket
+            if self.collide_widget(die_basket) and self not in die_basket.keepers:
+                die_basket.keepers.append(self)
+            if not self.collide_widget(die_basket) and self in die_basket.keepers:
+                die_basket.keepers.remove(self)
+            touch.ungrab(self)
 
 
 class Dice(Widget):
-
     def __init__(self, **kwargs):
         super(Dice, self).__init__(**kwargs)
+        self.id = 'dice'
 
     def update_dice(self, num_dice):
         roll = [random.randint(1, 6) for _ in range(6 - num_dice)]
         self.clear_widgets()
+
         sound = sounds[random.randint(1, 6)]
         sound.play()
+
         for x in range(len(roll)):
             image = Image(source=die_images[roll[x]])
-            scatter = Scatter(
-                center_x=self.parent.width * random.uniform(.1, .6),
-                center_y=self.parent.height * random.uniform(.1, .5))
-            new_die = Die(id=str(roll[x]))
-            new_die.add_widget(image)
-            scatter.add_widget(new_die)
+            scatter = DieScatter(
+                center_x=self.parent.width * .75,
+                center_y=self.parent.height * .1,
+                id=str(roll[x]))
+            scatter.add_widget(image)
             self.add_widget(scatter)
+
+        for child in self.parent.children:
+            if child.id == 'dice':
+                for kid in child.children:
+                    anim = Animation(center_x=self.parent.width * random.uniform(.1, .6),
+                                     center_y=self.parent.height * random.uniform(.1, .5),
+                                     d=.25)
+                    anim.start(kid)
 
 
 class EndTurn(Button):
@@ -430,8 +472,10 @@ class Roll(Button):
     def on_press(self):
         if self.parent.die_basket.valid_basket == [.4, .69, .3, 1]:
             self.keeper_count += len(self.parent.die_basket.keepers)
+
             if self.keeper_count >= 6:
                 self.keeper_count = 0
+
             self.parent.dice.update_dice(self.keeper_count)
             self.parent.die_basket.keepers.clear()
 
@@ -457,16 +501,19 @@ class DieBasket(BoxLayout):
         self.valid_basket = [.4, .69, .3, 1]
 
     def on_keepers(self, instance, value):
-        children = [child.children for child in self.keepers]
-        choice = [int(keeper[0].id) for keeper in children]
+        choice = [int(child.id) for child in self.keepers]
+
         score = self.active_game.keep_score(choice)
         self.basket_score = score
         self.parent.parent.update_display('basket')
+
         scored = self.active_game.validate_choice(choice)
         scored = not bool(scored)
+
         if not scored or not choice:
             self.valid_basket = [1, 0, 0, 1]
             self.parent.parent.ids['roll'].background_color = [.21, .45, .3, .5]
+
         if scored and choice:
             self.valid_basket = [.4, .69, .3, 1]
             self.parent.parent.ids['roll'].background_color = [.21, .45, .3, 1]
--- logic.py
+++ logic.py
@@ -1,46 +1,15 @@
 # Copyright 2018 Paul Kutrich. All rights reserved.
 
-from random import randint
 from collections import Counter
-from scoring_rules import scoring_rules, overlord_scoring_rules
-
-msg = '''
-                    Welcome to Ten Thousand
-                           The Game!
-Objective:
-    Score 10,000 points.
-    Any turn in which a player ends with more than 10,000 points will be the 
-    final turn.
-    If multiple players finish with more than 10,000 points, the winner is the
-    player with the most points.
-
-To score:
-    1's and 5's are worth 100 and 50 points respectively.
-    Three-of-a-kind is worth the die number x 100. (Three 1's is 1000 points)
-    Four-of-a-kind is worth double the same Three-of-a-kind.
-    Five-of-a-kind is double Four-of-a-kind.
-    Six of any number is worth 5000 points.
-    A six dice straight is worth 1500 points.
-    Three pairs are also worth 1500 points.
-
-To play:
-    Your dice will appear in [brackets].
-    Choose your dice by the reference number located above your dice.
-    You must choose at least one scoring die per throw. If no scoring die are
-    thrown your turn is over and your score for the turn will be zero.
-    You must score at least 500 points in one turn to get on the board.
-    If you get six keepers in a turn, you may choose to roll all dice again.
-    You'll press Enter a lot. Sorry about that. There will be graphics soon.
-    Have fun and thanks for helping me develop my first app!!
-'''
+from scoring_rules import scoring_rules
 
 
 class Game:
 
     """The nuts and bolts of the game.
 
-    Methods for instantiating players in player_list, game rules, scoring,
-    taking turns, choosing a winner, and ending the game.
+    Methods for instantiating players in player_list, game rules, scoring, taking turns, choosing a winner, and ending
+    the game.
 
     """
 
@@ -60,7 +29,7 @@ class Game:
         :return: List of Player objects
         """
         for name in name_list:
-            self.player_list.append(HumanPlayer(name))
+            self.player_list.append(Player(name))
         return self.player_list
 
     @staticmethod
@@ -93,70 +62,6 @@ class Game:
             return 0
         return score
 
-    def turn(self, player):
-        """One player turn.
-
-        Updates total scores. Sets thresholds for ComPlayer ending its turns.
-        Calls Player.choose_dice and Game.keep_score.
-
-        :param player: Player object.
-        """
-        round_score, keepers = 0, 0
-        while True:
-            roll = [randint(1, 6) for _ in range(6 - keepers)]
-            print(f'\n{player.name}, your dice in []'
-                  '\n 1  2  3  4  5  6'
-                  f'\n{roll}')
-            choice = player.choose_dice(roll)
-            print(f'\nHere are your choices: {choice}')
-            score = self.keep_score(choice)
-            if score == 0:
-                break
-            print(f'\nScore for this throw is: {score}')
-            round_score += score
-            print(f'\nTotal score for this turn is: {round_score}')
-            keepers += len(choice)
-            if keepers == 6:
-                print('\nSix keepers! Roll \'em again!')
-                keepers = 0
-            if player.name != 'Digital Overlord':
-                again = input('''
-Roll again or keep current score?
-Enter = Roll, K = Keep'''
-                              )
-                if r'k' in again.lower():
-                    player.total_score += round_score
-                    if player.total_score < 500:
-                        print('\nScore at least 500 to get on the board.')
-                        player.total_score = 0
-                        break
-                    else:
-                        print(f'\n{player.name}\'s score this turn: {round_score}')
-                        break
-            else:
-                if round_score >= 500 and keepers > 2:
-                    player.total_score += round_score
-                    print(f'\n{player.name}\'s score this turn: {round_score}')
-                    break
-
-    def take_turns(self, player_list):
-        """Sets winning score, switches between players, prints winner's name and score.
-
-        Calls Game.turn
-
-        :param player_list: List of Player objects.
-        """
-        while not any(player.total_score >= 10000 for player in player_list):
-            for player in player_list:
-                self.turn(player)
-                for _player in player_list:
-                    print(f'\n{_player.name}\'s total score is: {_player.total_score}')
-        winner_dict = {}
-        for x in range(len(player_list)):
-            winner_dict.update({player_list[x].total_score: player_list[x]})
-        winner = winner_dict[max(winner_dict.keys())]
-        print(f'\nThe winner is {winner.name} with a score of: {winner.total_score}!!')
-
     def validate_choice(self, choice):
         """Removes errant choices and informs user of error(s) if present.
 
@@ -176,12 +81,6 @@ Enter = Roll, K = Keep'''
                         Game.is_straight(choice) and not
                         Game.is_three_pair(choice)):
                     errors.append(die)
-        # for error in errors:
-        #     print(f'\nYou must have three {error}\'s to score. '
-        #           'Let\'s put that back.')
-        # choice = [die for die, count in counts.items()
-        #           for _ in range(count)
-        #           if scoring_rules[die - 1][count - 1] > 0]
         return errors
 
 
@@ -189,11 +88,11 @@ class Player(object):
 
     """Simple base class for Player objects.
 
-    Players have a total score and name.
+    Players have a total score, round score, and name.
 
     """
     def __init__(self, name):
-        """Player objects instantiated with total score and name.
+        """Player objects instantiated with total score, round score, and name.
 
         :param name: From Game.set_player.
         """
@@ -202,91 +101,8 @@ class Player(object):
         self.name = name
 
 
-class HumanPlayer(Player):
-
-    """Human Player objects inherits from Player base class.
-
-    Methods for obtaining user input and input validation.
-
-    """
-
-    def _get_user_choice(self):
-        """Adjusts user input for indexing. Returns input if valid.
-
-        :return: int. User choice adjusted for zero indexing.
-        """
-        try:
-            choose = int(input('''
-Choose which die to keep by position 1-6
-Type position number, then enter. Repeat for all choices.
-Press enter when finished
-'''
-                               ))
-        except ValueError:
-            return None
-        return choose - 1
-
-    def choose_dice(self, roll):
-        """Ensures user input refers to a valid choice.
-
-        Calls get_user_choice().
-        Calls validate_choice().
-        :param roll: List. Dice user can choose from.
-        :return: List. validated user choices.
-        """
-        choice_list = []
-        while True:
-            choose = self._get_user_choice()
-            if choose is None:  # From user pressing enter to stop choosing dice.
-                break
-            if choose >= len(roll):
-                print(f'\n{choose + 1} not available\n')
-                continue
-            if choose in choice_list:
-                print('\nYou may choose a die only once.\n')
-                continue
-            choice_list.append(choose)
-        choice = [roll[x] for x in choice_list]
-        return self._validate_choice(choice)
-
-
-
-class ComPlayer(Player):
-
-    """Computer player with method for choosing dice.
-
-    Inherits from Player base class.
-
-    """
-
-    def choose_dice(self, roll):
-        """Choose dice according to scoring rules. Boop Beep.
-
-        :param roll: List. Dice to choose from.
-        :return: List. Dice chosen.
-        """
-        counts = Counter(roll)
-        if Game.is_three_pair(roll) and \
-                sum(overlord_scoring_rules[die - 1][count - 1]
-                    for die, count in counts.items()) < 1500:
-            choice = roll
-        elif Game.is_straight(roll):
-            choice = roll
-        else:
-            choice = [die for die, count in counts.items() for _ in range(count)
-                      if overlord_scoring_rules[die - 1][count - 1] > 0]
-        return choice
-
-
 def main():
-    print(msg)
-    while True:
-        game = Game()
-        game.take_turns(game.player_list)
-        replay = input('\nWould you like to play again? y/n')
-        if r'n' in replay.lower():
-            print('Thanks for playing!!')
-            break
+    pass
 
 
 if __name__ == "__main__":
